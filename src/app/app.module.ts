1ee1966b58617c10bfe8ecb7678508956873f44b
Joel Matsu <matsu.joel@gmail.com>
2018-02-19 17:36:01 -0500

    Just created Login, Registration and Favorites components. Commented use of UrlParam component which apparently has no use and was preventing the navigation to second pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule, MdTooltipModule, MdButtonModule, MdDialogModule, MdMenuModule, MdIconModule, MdToolbarModule, MdProgressBarModule, MdProgressSpinnerModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -14,10 +14,20 @@ import { ScrollToModule } from 'ng2-scroll-to';
 import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ShareButtonsModule } from 'ngx-sharebuttons';
-import { UrlparamComponent } from './urlparam/urlparam.component';
+// import { UrlparamComponent } from './urlparam/urlparam.component';
 
 import 'hammerjs';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: ':id', component: UrlparamComponent }, // I don't know what is urlparam for, but it's preventing navigation to other pages
+  { path: 'login', component: LoginComponent },  
+  { path: 'registration', component: RegistrationComponent },  
+  { path: 'favorites', component: FavoritesComponent },  
+];
 
 @NgModule({
   declarations: [
@@ -25,14 +35,31 @@ import 'hammerjs';
     HomeComponent,
     HomePostComponent,
     EscapeHtmlPipe,
-    UrlparamComponent
+    // UrlparamComponent,
+    LoginComponent,
+    RegistrationComponent,
+    FavoritesComponent
   ],
-  imports: [ScrollToModule.forRoot(),
-    BrowserModule, RouterModule.forRoot([
-      { path: '', component: HomeComponent }, // Home component is called on root URL
-      { path: ':id', component: UrlparamComponent }
-    ]), HttpModule, BrowserAnimationsModule, FormsModule,
-        FlexLayoutModule, MdButtonModule, MdTooltipModule, MdDialogModule, MdMenuModule, MdIconModule, MdToolbarModule, MdProgressBarModule, MdProgressSpinnerModule, MaterialModule, MDBBootstrapModule.forRoot(), AngularFontAwesomeModule, ShareButtonsModule.forRoot()
+  imports: [
+    ScrollToModule.forRoot(),
+    RouterModule.forRoot(routes), 
+    BrowserModule, 
+    HttpModule, 
+    BrowserAnimationsModule, 
+    FormsModule,
+    FlexLayoutModule, 
+    MdButtonModule, 
+    MdTooltipModule, 
+    MdDialogModule, 
+    MdMenuModule, 
+    MdIconModule, 
+    MdToolbarModule, 
+    MdProgressBarModule,
+    MdProgressSpinnerModule, 
+    MaterialModule, 
+    MDBBootstrapModule.forRoot(), 
+    AngularFontAwesomeModule, 
+    ShareButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
