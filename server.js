491b4558fa192ab29f327956be8f20138537535f
Joel Matsu <matsu.joel@gmail.com>
2018-03-30 23:27:21 -0400

    Trying to fix reload of Favourites in Production

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+// Work around to be able to reload Favorites in Production
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
